cb0443f32a8a8f30aefd8d6dde8fd7436de02c19
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2025-03-20 16:03:36 +0200

    Add error handling and updata def and route names to more accurate

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 
 from model_training import train_model
 
@@ -8,15 +8,30 @@ app = Flask(__name__)
 def hello_world():
     return "<p> Hello World!</p>"
 
-@app.route('/data', methods=['POST'])
-def get_data():
-    model = train_model()
-    data = request.json
-    sentence = data["sentence"]
-    sentiment = model.predict([sentence])
-    print(sentiment)
-    return {"message": "Data received", "data": data}
-    
+@app.route('/evaluate', methods=['POST'])
+def evaluate():
+    try:
+        model = train_model()
+        data = request.json
+
+        if "sentence" not in data:
+            return jsonify({"error": "No sentence provided in the request"}), 400
+
+        sentence = data["sentence"]
+        result = model.predict([sentence])
+        sentiment = result[0]
+
+        response = {
+            "message": "Sentiment analysis complete",
+            "sentence": sentence,
+            "sentiment": sentiment
+        }
+
+        return jsonify(response)
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+        
 
 if __name__ == '__main__':
     app.run()
