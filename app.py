d7b25c2732f348dd23bbd81680146d9c80b67c6c
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2025-03-21 12:49:40 +0200

    Add CORS support

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 from model_training import train_model
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
