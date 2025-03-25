9dc199aa5a065852fe740fc661efae630e6a6b75
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2025-03-25 14:06:12 +0200

    Add .env and config.py to read and set environmental variables

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from model_training import train_model
+from config import Config
+
 
 app = Flask(__name__)
 CORS(app)
+
+JWT_SECRET = Config.JWT_SECRET
 
 @app.route('/')
 def hello_world():
