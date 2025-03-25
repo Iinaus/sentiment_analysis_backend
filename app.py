32d72731955b333facb64b23784d54f9d4633497
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2025-03-25 14:51:47 +0200

    Add username and password_hash to env. Create login-function to create token, if username and password matches

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import jwt
+import datetime
+import bcrypt
 
 from model_training import train_model
 from config import Config
@@ -9,10 +12,24 @@ app = Flask(__name__)
 CORS(app)
 
 JWT_SECRET = Config.JWT_SECRET
+USERNAME = Config.USERNAME
+PASSWORD_HASH = Config.PASSWORD_HASH
 
 @app.route('/')
 def hello_world():
     return "<p> Hello World! Webhook test</p>"
+
+@app.route('/login', methods=['POST'])
+def login():
+    auth = request.json
+    if auth and auth.get("username") == USERNAME:
+        if bcrypt.checkpw(auth.get("password").encode('utf-8'), PASSWORD_HASH.encode('utf-8')):
+            token = jwt.encode(
+                {'user': auth["username"], 'exp': datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(hours=1)},
+                JWT_SECRET, algorithm="HS256"
+            )
+            return jsonify({"token": token})
+    return jsonify({"error": "Invalid credentials"}), 401
 
 @app.route('/evaluate', methods=['POST'])
 def evaluate():
--- config.py
+++ config.py
@@ -5,3 +5,5 @@ load_dotenv()
 
 class Config:
     JWT_SECRET = os.getenv('JWT_SECRET')
+    USERNAME = os.getenv('USERNAME')
+    PASSWORD_HASH = os.getenv('PASSWORD_HASH')
